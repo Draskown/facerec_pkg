a750cfce3acaf18e4da2753b32c110a90b81a69e
Draskown <andrey2000534@gmail.com>
2023-05-30 16:34:40 +0300

    Added text to speech for greet user node

--- mngr_fr_pkg/greet_user.py
+++ mngr_fr_pkg/greet_user.py
@@ -1,5 +1,9 @@
 import sys
+import time
 import rclpy
+import pygame
+from gtts import gTTS
+from io import BytesIO
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -19,6 +23,10 @@ class GreetUser(Node):
             10,
         )
 
+        # Initialize pygame and pygame mixer instances
+        pygame.init()
+        pygame.mixer.init()
+
     def __callback(self, 
                     msg: String,
                     ) -> None:
@@ -27,7 +35,32 @@ class GreetUser(Node):
         """ 
         
         # Greet the user
-        sys.stdout.write("Hello there, " + msg.data + "\n")
+        self.__speak_text("Hello there, " + msg.data)
+
+    def __speak_text(self, text):
+        """
+        Voices the passed text
+
+        Args:
+        - text: the text to voice
+        """
+        # Create an io object to store the voicing
+        io = BytesIO()
+        # Create a voicer instance
+        tts = gTTS(text, lang="ru")
+        # Write generated audio into the io
+        tts.write_to_fp(io)
+        # Initialize the first input of the io
+        io.seek(0)
+
+        # Create a sound object of the io
+        sound = pygame.mixer.Sound(io)
+        # Voice the text
+        sound.play()
+
+        # Wait until the whole text is said
+        while pygame.mixer.get_busy():
+            time.sleep(1)
 
 
 def main(args: list=None) -> None:
